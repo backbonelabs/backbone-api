de7cc10dabff51eaad730f681dbc01ef6ca1fa67
Kevin Huang <huang.kev@gmail.com>
2016-08-05 00:38:53 -0700

    Include access to mongodb library in dbManager

--- routes/users/getUser.js
+++ routes/users/getUser.js
@@ -4,7 +4,7 @@ export default req => {
   const id = req.params.id;
   return dbManager.getDb()
     .collection('users')
-    .findOne({ _id: id })
+    .findOne({ _id: dbManager.mongodb.ObjectId(id) })
     .then(user => {
       if (user) {
         return user;
