f9efa40c24b1521fdbdc50bf5912212d0dccaa87
Kevin Huang <huang.kev@gmail.com>
2016-08-05 01:21:25 -0700

    Add middleware for requiring auth to access routes

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,11 +1,12 @@
 import Router from 'express';
+import requireAuth from '../../lib/requireAuth';
 import handleRoute from '../../lib/handleRoute';
 import getUser from './getUser';
 import createUser from './createUser';
 
 const router = Router();
 
-router.get('/:id', handleRoute(getUser));
+router.get('/:id', requireAuth, handleRoute(getUser));
 router.post('/', handleRoute(createUser));
 
 export default router;
