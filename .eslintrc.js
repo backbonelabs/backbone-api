6de89a9bc480d9af17964d6b81f65bec187316ed
Kevin Huang <huang.kev@gmail.com>
2016-08-09 15:28:01 -0700

    Enable object rest spread linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: 'airbnb-base',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
   rules: {
     'new-cap': 0,
     'no-underscore-dangle': 0
