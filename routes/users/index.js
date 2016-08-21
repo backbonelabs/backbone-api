d995956afd4a3b6a66f035ea55aa7f9fc2b2ea8a
Khoa Phan <kp@gobackbone.com>
2016-08-21 16:07:36 -0700

    Add route to handle email confirmation link

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,10 +3,12 @@ import requireAuth from '../../lib/requireAuth';
 import handleRoute from '../../lib/handleRoute';
 import getUser from './getUser';
 import createUser from './createUser';
+import confirmUser from './confirmUser';
 import updateUser from './updateUser';
 
 const router = Router();
 
+router.get('/confirm', handleRoute(confirmUser));
 router.get('/:id', requireAuth, handleRoute(getUser));
 router.post('/:id', requireAuth, handleRoute(updateUser));
 router.post('/', handleRoute(createUser));
