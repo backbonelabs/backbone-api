b6c3f6f3e50d023e786c2026b7fd8c3f378c804d
Khoa Phan <kp@gobackbone.com>
2016-08-24 19:39:51 -0700

    Create files for `resendConfirmation` route

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,12 +5,14 @@ import getUser from './getUser';
 import createUser from './createUser';
 import updateUser from './updateUser';
 import getUserConfirmed from './getUserConfirmed';
+import resendConfirmation from './resendConfirmation';
 
 const router = Router();
 
-router.post('/', handleRoute(createUser));
 router.get('/:id', requireAuth, handleRoute(getUser));
-router.post('/:id', requireAuth, handleRoute(updateUser));
 router.get('/confirm/:email', handleRoute(getUserConfirmed));
+router.post('/', handleRoute(createUser));
+router.post('/resend', handleRoute(resendConfirmation));
+router.post('/:id', requireAuth, handleRoute(updateUser));
 
 export default router;
---
+++ routes/users/resendConfirmation.js
@@ -0,0 +1,23 @@
+import dbManager from '../../lib/dbManager';
+import emailUtility from '../../lib/emailUtility';
+
+export default req => {
+  console.log('req body ', req.body);
+  const email = req.body.email;
+  const confirmationTokenExpiry = new Date();
+  confirmationTokenExpiry.setDate(confirmationTokenExpiry.getDate() + 2);
+
+  return emailUtility.generateConfirmationToken()
+  .then((token) => (
+    dbManager.getDb()
+    .collection('users')
+    .update({ email },
+      { $set: {
+        confirmationToken: token,
+        confirmationTokenExpiry,
+      },
+    })
+    .then(() => emailUtility.sendEmailConfirmation(email, token))
+  ))
+  .then(() => true);
+};
