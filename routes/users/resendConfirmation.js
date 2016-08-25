db098f83ff647ca4d7630606d9a4d602a48d3437
Khoa Phan <kp@gobackbone.com>
2016-08-25 10:38:48 -0700

    Check if user exists when checking when resending a confirmation

--- routes/users/resendConfirmation.js
+++ routes/users/resendConfirmation.js
@@ -1,22 +1,34 @@
 import dbManager from '../../lib/dbManager';
-import emailUtility from '../../lib/emailUtility';
+import emailService from '../../lib/emailService';
+import tokenFactory from '../../lib/tokenFactory';
 
-export default req => {
+export default (req, res) => {
   const email = req.body.email;
-  const confirmationTokenExpiry = new Date();
-  confirmationTokenExpiry.setDate(confirmationTokenExpiry.getDate() + 2);
 
-  return emailUtility.generateConfirmationToken()
-  .then((token) => (
-    dbManager.getDb()
-    .collection('users')
-    .update({ email },
-      { $set: {
-        confirmationToken: token,
-        confirmationTokenExpiry,
-      },
-    })
-    .then(() => emailUtility.sendConfirmationEmail(email, token))
-  ))
-  .then(() => true);
+  return dbManager.getDb()
+  .collection('users')
+  .findOne({ email })
+  .then((user) => {
+    if (!user) {
+      res.status(400);
+      throw new Error('Account not found, please sign-up again');
+    } else {
+      const confirmationTokenExpiry = new Date();
+      confirmationTokenExpiry.setDate(confirmationTokenExpiry.getDate() + 2);
+
+      return tokenFactory.generateConfirmationToken()
+      .then((confirmationToken) => (
+        dbManager.getDb()
+        .collection('users')
+        .updateOne({ email },
+          { $set: {
+            confirmationToken,
+            confirmationTokenExpiry,
+          },
+        })
+        .then(() => emailService.sendConfirmationEmail(email, confirmationToken))
+      ))
+      .then(() => true);
+    }
+  });
 };
