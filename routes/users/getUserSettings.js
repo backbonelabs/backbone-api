7d2896df60ff0cc786fef03bb0e1b5a31418bef0
Khoa Phan <kp@gobackbone.com>
2016-09-01 23:10:08 -0700

    Create routes for handling getting and updating user settings

---
+++ routes/users/getUserSettings.js
@@ -0,0 +1,24 @@
+import dbManager from '../../lib/dbManager';
+import sanitizeUser from '../../lib/sanitizeUser';
+
+export default req =>
+  dbManager.getDb()
+    .collection('users')
+    .findOne({ _id: dbManager.mongodb.ObjectId(req.params.id) })
+    .then(user => {
+      if (user && user.settings) {
+        // Return user object without password
+        return sanitizeUser(user);
+      }
+
+      const settings = { postureThreshold: 0.1 };
+
+      return dbManager.getDb()
+        .collection('users')
+        .updateOne(
+          { _id: dbManager.mongodb.ObjectId(req.params.id) },
+          { settings },
+          { upsert: true }
+        )
+        .then(() => Object.assign({}, user, settings));
+    });
---
+++ routes/users/updateUserSettings.js
@@ -0,0 +1,27 @@
+import schemas from '../../lib/schemas';
+import validate from '../../lib/validate';
+import dbManager from '../../lib/dbManager';
+import sanitizeUser from '../../lib/sanitizeUser';
+
+export default req => validate(req.body, Object.assign({},
+  schemas.user), [], ['_id'])
+  .then(() =>
+    dbManager.getDb()
+      .collection('users')
+      .updateOne(
+        { _id: dbManager.mongodb.ObjectID(req.params.id) },
+        { $set: { settings: req.body.settings } }
+      )
+  )
+  .then(updateWriteOpResult => {
+    if (!updateWriteOpResult.modifiedCount) {
+      // User ID doesn't exist
+      throw new Error('Invalid user');
+    }
+
+    // Get updated user document
+    return dbManager.getDb()
+      .collection('users')
+      .findOne({ _id: dbManager.mongodb.ObjectID(req.params.id) });
+  })
+  .then(user => sanitizeUser(user));
