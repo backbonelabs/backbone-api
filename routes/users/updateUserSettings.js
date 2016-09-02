423b9dd624bccb33f15b95a96bd88afa8957f610
Khoa Phan <kp@gobackbone.com>
2016-09-02 15:55:12 -0700

    Remove last two arguments

--- routes/users/updateUserSettings.js
+++ routes/users/updateUserSettings.js
@@ -3,7 +3,7 @@ import validate from '../../lib/validate';
 import dbManager from '../../lib/dbManager';
 import sanitizeUser from '../../lib/sanitizeUser';
 
-export default req => validate(req.body, schemas.settings, [], ['_id'])
+export default req => validate(req.body, schemas.settings)
   .then(() => {
     const settings = { settings: req.body };
 
