32b0465aff1cd841690e1897f7080688e2e837a1
Kevin Huang <huang.kev@gmail.com>
2016-09-02 20:23:56 -0700

    Use findOneAndUpdate
    This will allow us to update a document in the database and receive the updated document

--- routes/users/getUserSettings.js
+++ routes/users/getUserSettings.js
@@ -1,7 +1,7 @@
 import dbManager from '../../lib/dbManager';
 import sanitizeUser from '../../lib/sanitizeUser';
 
-export default req =>
+export default req => (
   dbManager.getDb()
     .collection('users')
     .findOne({ _id: dbManager.mongodb.ObjectId(req.params.id) })
@@ -9,16 +9,29 @@ export default req =>
       if (!user) {
         throw new Error('No user found');
       } else if (user && user.settings) {
+        // User exists and has existing settings
         // Return user object without password
         return sanitizeUser(user);
       }
+      // User exists but does not have any existing settings
+      // Set default settings
       const settings = { settings: { postureThreshold: 0.2 } };
 
       return dbManager.getDb()
         .collection('users')
-        .updateOne(
+        .findOneAndUpdate(
           { _id: dbManager.mongodb.ObjectId(req.params.id) },
-          { $set: settings }
+          { $set: settings },
+          { returnOriginal: false }
         )
-        .then(() => Object.assign({}, user, settings));
-    });
+        .then(result => {
+          if (!result.value) {
+            // User ID doesn't exist
+            throw new Error('Invalid user');
+          }
+
+          // Return updated user
+          return sanitizeUser(result.value);
+        });
+    })
+);
