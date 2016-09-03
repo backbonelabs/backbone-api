c2657322ac6e72abfa3d9bbad7282f1c9804cd01
Kevin Huang <huang.kev@gmail.com>
2016-09-02 21:18:24 -0700

    Use default values for missing settings

--- routes/users/getUserSettings.js
+++ routes/users/getUserSettings.js
@@ -1,8 +1,10 @@
+import { get, isEqual } from 'lodash';
 import dbManager from '../../lib/dbManager';
-import defaultUserSettings from '../../lib/defaultUserSettings';
+import userSettings from '../../lib/userSettings';
 
 /**
- * Returns settings for a user. If no settings exist, default settings will be applied to the user.
+ * Returns settings for a user. If the user is missing settings, default values for the
+ * missing settings will be merged with the user's existing settings.
  * @param  {Object} req Request
  * @return {Promise} Resolves with an object representing the user's settings
  */
@@ -13,30 +15,34 @@ export default req => (
     .then(user => {
       if (!user) {
         throw new Error('No user found');
-      } else if (user && user.settings) {
-        // User exists and has existing settings
-        // Return existing user settings
-        return user.settings;
+      } else {
+        // Get user's existing settings
+        const existingSettings = get(user, 'settings', {});
+        // Merge user's settings with default settings
+        const mergedSettings = userSettings.mergeWithDefaults(existingSettings);
+
+        if (isEqual(existingSettings, mergedSettings)) {
+          // User already has all settings defined
+          return existingSettings;
+        }
+
+        // User is missing settings, fill missing settings with default values
+        return dbManager.getDb()
+          .collection('users')
+          .findOneAndUpdate(
+            { _id: dbManager.mongodb.ObjectId(req.params.id) },
+            { $set: { settings: mergedSettings } },
+            { returnOriginal: false }
+          )
+          .then(result => {
+            if (!result.value) {
+              // User ID doesn't exist
+              throw new Error('Invalid user');
+            }
+
+            // Return user settings
+            return result.value.settings;
+          });
       }
-      // User exists but does not have any existing settings
-      // Set default settings
-      const settings = { settings: defaultUserSettings };
-
-      return dbManager.getDb()
-        .collection('users')
-        .findOneAndUpdate(
-          { _id: dbManager.mongodb.ObjectId(req.params.id) },
-          { $set: settings },
-          { returnOriginal: false }
-        )
-        .then(result => {
-          if (!result.value) {
-            // User ID doesn't exist
-            throw new Error('Invalid user');
-          }
-
-          // Return user settings
-          return result.value.settings;
-        });
     })
 );
