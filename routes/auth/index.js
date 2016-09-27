be7655dd72fd64dc2686ed304662db07568b9ae5
Khoa Phan <kp@gobackbone.com>
2016-09-27 11:59:47 -0700

    Recover email confirmation routes/code to confirm new emails

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,10 +3,12 @@ import handleRoute from '../../lib/handleRoute';
 import requireAuth from '../../lib/requireAuth';
 import login from './login';
 import logout from './logout';
+import confirm from './confirm';
 
 const router = Router();
 
 router.post('/login', handleRoute(login));
 router.post('/logout', requireAuth, handleRoute(logout));
+router.get('/confirm', handleRoute(confirm));
 
 export default router;
--- routes/users/index.js
+++ routes/users/index.js
@@ -5,6 +5,7 @@ import getUser from './getUser';
 import createUser from './createUser';
 import updateUser from './updateUser';
 import updateUserSettings from './updateUserSettings';
+import getUserConfirmed from './getUserConfirmed';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.post('/', handleRoute(createUser));
 router.get('/:id', requireAuth, handleRoute(getUser));
 router.post('/:id', requireAuth, handleRoute(updateUser));
 router.post('/settings/:id', requireAuth, handleRoute(updateUserSettings));
+router.get('/confirm/:email', handleRoute(getUserConfirmed));
 
 export default router;
