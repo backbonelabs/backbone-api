ae63b94ece9e9014888a459cd3c3c1263a9d8fd6
Kevin Huang <huang.kev@gmail.com>
2017-03-03 20:01:45 -0800

    Convert dates to Pacific for Mixpanel API

--- routes/users/getUserSessions.js
+++ routes/users/getUserSessions.js
@@ -1,25 +1,34 @@
+import Debug from 'debug';
 import request from 'request-promise';
 import errors from 'request-promise/errors';
-
-import Debug from 'debug';
+import moment from 'moment-timezone';
 
 const debug = Debug('routes:users:getUserSessions');
 
 /**
  * Returns user posture sessions for the given dates
- * @param  {Object} req             Request
- * @param  {Object} req.params      Request parameters
- * @param  {String} req.query.from  Date
- * @param  {String} req.query.to    Date
- * @param  {String} req.params.id   User ID
- * @return {Promise} Resolves with the sessions object
+ * @param  {Object} req            Request
+ * @param  {Object} req.params     Request route parameters
+ * @param  {String} req.params.id  User ID
+ * @param  {Object} req.query      Request query parameters
+ * @param  {String} req.query.from Starting date of posture sessions to retrieve.
+ *                                        It must match known ISO 8601 formats.
+ * @param  {String} req.query.to   Ending date of posture session to retrieve.
+ *                                        It must match known ISO 8601 formats.
+ * @return {Promise<Array>} Resolves with an array of the sessions within the date range
  */
 
 export default (req, res) => {
   const { id } = req.params;
   const { from, to } = req.query;
 
-/* eslint-disable */
+  // Mixpanel stores, queries, and returns event times in our project's timezone,
+  // which is US/Pacific. As a result, we need to convert the query dates to
+  // Pacific time. See https://blog.mixpanel.com/2015/08/24/community-tip-all-about-time.
+  const fromDateString = moment.tz(from, 'UTC').tz('US/Pacific').format('YYYY-MM-DD');
+  const toDateString = moment.tz(to, 'UTC').tz('US/Pacific').format('YYYY-MM-DD');
+
+  /* eslint-disable */
   const script = function main() {
     return Events({
       from_date: params.from_date,
@@ -28,14 +37,14 @@ export default (req, res) => {
       return (event.name === 'postureSession') && (event.distinct_id === params.id);
     });
   };
-/* eslint-disable */
+  /* eslint-disable */
 
   return request.post('https://mixpanel.com/api/2.0/jql/', {
     form: {
       script: script.toString(),
       params: JSON.stringify({
-        from_date: from,
-        to_date: to,
+        from_date: fromDateString,
+        to_date: toDateString,
         id,
       }),
     },
