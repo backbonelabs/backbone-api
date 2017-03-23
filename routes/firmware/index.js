8904682eeeaeba0859a1bb3bfa274a1d36072093
Tuan Ho <tuan@gobackbone.com>
2017-03-23 13:16:24 -0700

    Added routing for specific firmware version

--- routes/firmware/index.js
+++ routes/firmware/index.js
@@ -3,10 +3,25 @@ import handleRoute from '../../lib/handleRoute';
 
 const router = Router();
 
+// Retuns the latest version for a specific version and the file url
+const handleFirmwareVersions = (req, res) => {
+
+    // Extracts requested version from URL
+    const reqVersion = req.url.match(/\d+/);
+    const version = process.env[`BL_LATEST_FIRMWARE_VERSION_${reqVersion}`];
+    const baseUrl = process.env.BL_FIRMWARE_URL;
+    const url = `${firmwareUrl}Backbone_${version}.cyacd`;
+
+    return { version, url };
+};
+
 // Returns information about the latest firmware
-router.get(/v\d+/, handleRoute(() => ({
+router.get('/', handleRoute(() => ({
   version: process.env.BL_LATEST_FIRMWARE_VERSION,
   url: process.env.BL_LATEST_FIRMWARE_URL,
 })));
+
+// Returns information for a specific firmware version from URL
+router.get(/v\d+/, handleRoute(handleFirmwareVersions));
 
 export default router;
