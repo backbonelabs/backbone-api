272209a986c61480acdb91f03d8452754d4b3f2c
Tuan Ho <tuan@gobackbone.com>
2017-03-24 12:47:36 -0700

    Fixed linting problems

--- test/integration/firmware2.js
+++ test/integration/firmware2.js
@@ -13,26 +13,24 @@ before(() => Promise.resolve(server)
 
 describe('/firmware router', () => {
   describe('GET /', () => {
-    const url = '/firmware';
     it('should return the latest firmware details', (done) => {
-      for(var key in process.env) {
-        if(/BL_LATEST_FIRMWARE_VERSION_/.test(key)) {
-          const baseUrl = '/firmware/v';
-          const firmware = process.env[key];
-          const majorSoftwareVersion = firmware.split('.')[2];
-          const testUrl = `${baseUrl}${majorSoftwareVersion}`;
-          const baseFileUrl = process.env.BL_FIRMWARE_URL;
-          const fileUrl = `${baseFileUrl}Backbone_${firmware}.cyacd`;
-          request(app)
-            .get(testUrl)
-            .send({})
-            .expect(200)
-            .expect({
-              version: process.env[key],
-              url: fileUrl,
-            });
-        }
-      }
+      const fw = Object.keys(process.env).filter(v => /BL_LATEST_FIRMWARE_VERSION_/.test(v));
+      fw.forEach((v) => {
+        const baseUrl = '/firmware/v';
+        const firmware = process.env[v];
+        const majorSoftwareVersion = firmware.split('.')[2];
+        const testUrl = `${baseUrl}${majorSoftwareVersion}`;
+        const baseFileUrl = process.env.BL_FIRMWARE_URL;
+        const fileUrl = `${baseFileUrl}Backbone_${firmware}.cyacd`;
+        request(app)
+          .get(testUrl)
+          .send({})
+          .expect(200)
+          .expect({
+            version: firmware,
+            url: fileUrl,
+          });
+      });
       done();
     });
   });
