9587c00cd4c0d8a2b596d0ad08a8dd42a2c22f8d
Kevin Huang <huang.kev@gmail.com>
2017-04-28 22:16:45 -0700

    Refactor admin login and logout routes

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,81 +1,95 @@
 import Debug from 'debug';
+import GoogleAuth from 'google-auth-library';
 import validate from '../../lib/validate';
 import schemas from '../../lib/schemas';
 import dbManager from '../../lib/dbManager';
-import passwordUtil from '../../lib/password';
-import sanitizeUser from '../../lib/sanitizeUser';
 import tokenFactory from '../../lib/tokenFactory';
 
 const debug = Debug('routes:admin:login');
-const errorMessage = 'Invalid login credentials. Please try again.';
+
+const auth = new GoogleAuth();
+const client = new auth.OAuth2(process.env.GOOGLE_CLIENT_ID);
 
 /**
- * Verifies an internal user account by checking the email and password and returns the
- * user object. A new access token is also regenerated and stored with the user. The access
- * token can be used in subsequent requests to access protected API endpoints.
+ * Verifies an internal user using their Google Auth ID token obtained from the
+ * client side. For a valid ID token, an access token is created for the internal
+ * user which can be later used to access protected admin routes. The access token
+ * along with their Google user account info is returned in the response.
  *
- * @param  {Object} req               Request
- * @param  {Object} req.body          Request body
- * @param  {String} req.body.email    Email address of the user
- * @param  {String} req.body.password Password of the user
- * @return {Promise} Resolves with a user object that has an accessToken property
+ * https://developers.google.com/identity/sign-in/web/backend-auth
+ * @param {Object} req              Request
+ * @param {Object} res              Response
+ * @param {Object} req.body         Request body
+ * @param {String} req.body.idToken Google ID token
+ * @return {Promise} Resolves with a user object containing an access token
  */
 export default (req, res) => validate(req.body, {
-  email: schemas.user.email,
-  password: schemas.password,
-}, ['email', 'password'])
-  .then(() => {
-    const { email } = req.body;
+  idToken: schemas.googleIdToken,
+}, ['idToken'])
+  .then(() => new Promise((resolve, reject) => {
+    debug('Verifying ID token', req.body.idToken);
+    client.verifyIdToken(req.body.idToken, process.env.GOOGLE_CLIENT_ID, (err, login) => {
+      if (err) {
+        debug('Error verifying id token', err);
+        res.status(500);
+        reject(err);
+      } else if (!login || !login.getPayload) {
+        debug('Unknown login response from verifyIdToken');
+        res.status(500);
+        reject(new Error('Unknown login response from verifyIdToken'));
+      } else {
+        const {
+          sub: id,
+          name,
+          given_name: firstName,
+          family_name: lastName,
+          picture,
+          email,
+          hd: domain,
+        } = login.getPayload();
 
-    // Look up user by email
-    return dbManager.getDb()
-      .collection('internalUsers')
-      .find({ email: new RegExp(email, 'i') })
-      .limit(1)
-      .next()
-      .then((user) => {
-        if (user) {
-          debug('Found internal user by email', email);
-          return user;
+        // Verify Google account belongs to gobackbone.com
+        if (domain !== 'gobackbone.com') {
+          res.status(401);
+          reject(new Error('Unauthorized account'));
+        } else {
+          const user = {
+            id,
+            name,
+            firstName,
+            lastName,
+            picture,
+            email,
+          };
+          debug('Valid Google account', user);
+          resolve(user);
         }
-        debug('Did not find internal user by email', email);
-        throw new Error(errorMessage);
-      });
+      }
+    });
   })
-  .then(user => (
-    // Verify password matches
-    Promise.all([user, passwordUtil.verify(req.body.password, user.password)])
-  ))
-  .then(([user, isPasswordMatch]) => {
-    if (!isPasswordMatch) {
-      debug('Invalid password');
-      throw new Error(errorMessage);
-    }
-    return user;
-  })
-  .then((user) => {
-    // Generate an access token
-    const { _id: userId } = user;
+    .then((user) => {
+      // Generate an access token for the user
+      const email = user.email;
+      return Promise.all([user, tokenFactory.createAccessToken(email)]);
+    })
+    .then(([user, accessToken]) => {
+      const {
+        id: googleId,
+        name,
+        firstName,
+        lastName,
+        email,
+        picture,
+      } = user;
 
-    return tokenFactory.createAccessToken(userId)
-      .then((accessToken) => {
-        debug('Generated access token', accessToken);
-
-        // Store access token with user
-        return dbManager.getDb()
-          .collection('internalUsers')
-          .findOneAndUpdate(
-            { _id: dbManager.mongodb.ObjectId(userId) },
-            { $set: { accessToken } },
-            { returnOriginal: false }
-          );
-      });
-  })
-  .then(result => sanitizeUser(result.value))
-  .catch((err) => {
-    if (err.message === errorMessage) {
-      // Use 401 status code for invalid auth errors
-      res.status(401);
-    }
-    throw err;
-  });
+      // Store Google user info along with access token
+      return dbManager.getDb()
+        .collection('internalUsers')
+        .findOneAndUpdate(
+          { email },
+          { $set: { googleId, name, firstName, lastName, email, picture, accessToken } },
+          { upsert: true, returnOriginal: false }
+        );
+    })
+    .then(result => result.value)
+  );
--- routes/admin/logout.js
+++ routes/admin/logout.js
@@ -1,29 +1,33 @@
-import validate from '../../lib/validate';
-import schemas from '../../lib/schemas';
+import Debug from 'debug';
 import dbManager from '../../lib/dbManager';
+
+const debug = Debug('routes:admin:logout');
 
 /**
  * Removes the access token for an internal user so subsequent API requests
- * using the access token will not be accepted.
+ * using the access token will not be allowed.
  * @param  {Object} req                  Request
  * @param  {Object} req.body             Request body
  * @param  {String} req.body.accessToken Access token to remove
  * @return {Promise} Resolves with undefined
  */
-export default req => validate(req.body, {
-  accessToken: schemas.accessToken,
-}, ['accessToken'])
-  .then(() => {
-    const { accessToken } = req.body;
-    return dbManager.getDb()
-      .collection('internalUsers')
-      .findOneAndUpdate(
-        { accessToken },
-        { $unset: { accessToken: true } },
-        { returnOriginal: false },
-      )
-      .then(() => {
-        // Nothing needs to be returned
-        return;
-      });
+export default req => new Promise((resolve, reject) => {
+  const accessToken = req.headers.authorization.split(' ')[1];
+  debug('Processing logout request for access token', accessToken);
+  dbManager.getDb()
+    .collection('internalUsers')
+    .findOneAndUpdate(
+      { accessToken },
+      { $unset: { accessToken: true } },
+      { returnOriginal: false },
+    )
+    .then(() => {
+      debug('Logout successful');
+      // Nothing needs to be returned
+      resolve();
+    })
+    .catch((err) => {
+      debug('Error', err);
+      reject(err);
+    });
 });
