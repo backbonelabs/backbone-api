6f60345c595671cd528c054e935a5135cb309d70
Kevin Huang <huang.kev@gmail.com>
2017-05-02 15:44:52 -0700

    Add admin route for updating a user

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,11 +4,13 @@ import requireAdminAuth from '../../lib/requireAdminAuth';
 import login from './login';
 import logout from './logout';
 import getUsers from './getUsers';
+import updateUser from '../users/updateUser';
 
 const router = Router();
 
 router.post('/login', handleRoute(login));
 router.post('/logout', handleRoute(logout));
 router.get('/users', requireAdminAuth, handleRoute(getUsers));
+router.post('/users/:id', requireAdminAuth, handleRoute(updateUser));
 
 export default router;
