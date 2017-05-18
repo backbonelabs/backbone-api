96399c75e58aa0a3331f40094b1d61827c123c8f
Kevin Huang <huang.kev@gmail.com>
2017-05-17 18:21:36 -0700

    Use support@gobackbone.com as the default from

--- start.js
+++ start.js
@@ -9,7 +9,6 @@ const EmailUtility = require('./lib/EmailUtility').default;
 EmailUtility.init({
   apiKey: process.env.BL_MAILGUN_API,
   domain: process.env.BL_MAILGUN_DOMAIN,
-  fromAddress: `Backbone <hello@${process.env.BL_MAILGUN_DOMAIN}>`,
   silentEmail: process.env.BL_SILENT_EMAIL === 'true',
   useTestEmail: process.env.BL_USE_TEST_EMAIL === 'true',
   testEmail: process.env.BL_TEST_EMAIL,
