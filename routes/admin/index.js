c5af25ca23ea6db2294036e5e211133a8593e83f
Kevin Huang <huang.kev@gmail.com>
2017-05-23 23:20:56 -0700

    Add GET /admin/users/:id route for retrieving a user

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,6 +3,7 @@ import handleRoute from '../../lib/handleRoute';
 import requireAdminAuth from '../../lib/requireAdminAuth';
 import login from './login';
 import logout from './logout';
+import getUser from '../users/getUser';
 import getUsers from './getUsers';
 import updateUser from '../users/updateUser';
 
@@ -11,6 +12,7 @@ const router = Router();
 router.post('/login', handleRoute(login));
 router.post('/logout', handleRoute(logout));
 router.get('/users', requireAdminAuth, handleRoute(getUsers));
+router.get('/users/:id', requireAdminAuth, handleRoute(getUser));
 router.post('/users/:id', requireAdminAuth, handleRoute(updateUser));
 
 export default router;
