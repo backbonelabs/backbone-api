699547a2a05fc2e99be2213d1c5a2e7b0823318e
Kevin Huang <huang.kev@gmail.com>
2017-06-22 16:03:51 -0700

    Revert "Allow selective settings to be updated"
    This reverts commit 5b41a1bc016f43b33e135ecaa5f33ec875ed3011.

--- routes/users/updateUserSettings.js
+++ routes/users/updateUserSettings.js
@@ -1,17 +1,15 @@
-import mapKeys from 'lodash/mapKeys';
 import schemas from '../../lib/schemas';
 import validate from '../../lib/validate';
 import dbManager from '../../lib/dbManager';
 
 /**
  * Updates feature settings for a user
- * @param  {Object} req      Request
- * @param  {Object} req.body Request body containing the settings to update
+ * @param  {Object} req Request
  * @return {Promise} Resolves with the user object containing the updated attributes, sans password
  */
 export default req => validate(req.body, schemas.settings)
   .then(() => {
-    const settings = mapKeys(req.body, (value, key) => `settings.${key}`);
+    const settings = { settings: req.body };
 
     return dbManager.getDb()
       .collection('users')
