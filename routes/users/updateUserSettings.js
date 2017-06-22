5b41a1bc016f43b33e135ecaa5f33ec875ed3011
Kevin Huang <huang.kev@gmail.com>
2017-06-21 17:12:28 -0700

    Allow selective settings to be updated

--- routes/users/updateUserSettings.js
+++ routes/users/updateUserSettings.js
@@ -1,15 +1,17 @@
+import mapKeys from 'lodash/mapKeys';
 import schemas from '../../lib/schemas';
 import validate from '../../lib/validate';
 import dbManager from '../../lib/dbManager';
 
 /**
  * Updates feature settings for a user
- * @param  {Object} req Request
+ * @param  {Object} req      Request
+ * @param  {Object} req.body Request body containing the settings to update
  * @return {Promise} Resolves with the user object containing the updated attributes, sans password
  */
 export default req => validate(req.body, schemas.settings)
   .then(() => {
-    const settings = { settings: req.body };
+    const settings = mapKeys(req.body, (value, key) => `settings.${key}`);
 
     return dbManager.getDb()
       .collection('users')
